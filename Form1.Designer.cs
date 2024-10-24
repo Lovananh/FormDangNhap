eac48d525621fa0562a5ef6cdbc16707efae1d37
Lò Văn Anh <anhlo2819@gmail.com>
2024-10-24 09:03:36 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace FormDangNhap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTendangnhap = new System.Windows.Forms.TextBox();
+            this.txtMatkhau = new System.Windows.Forms.TextBox();
+            this.lblTendangnhap = new System.Windows.Forms.Label();
+            this.lblMatkhau = new System.Windows.Forms.Label();
+            this.btnDangnhap = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTendangnhap
+            // 
+            this.txtTendangnhap.Location = new System.Drawing.Point(351, 89);
+            this.txtTendangnhap.Name = "txtTendangnhap";
+            this.txtTendangnhap.Size = new System.Drawing.Size(236, 26);
+            this.txtTendangnhap.TabIndex = 0;
+            // 
+            // txtMatkhau
+            // 
+            this.txtMatkhau.Location = new System.Drawing.Point(351, 156);
+            this.txtMatkhau.Name = "txtMatkhau";
+            this.txtMatkhau.PasswordChar = '*';
+            this.txtMatkhau.Size = new System.Drawing.Size(236, 26);
+            this.txtMatkhau.TabIndex = 1;
+            // 
+            // lblTendangnhap
+            // 
+            this.lblTendangnhap.AutoSize = true;
+            this.lblTendangnhap.Location = new System.Drawing.Point(203, 95);
+            this.lblTendangnhap.Name = "lblTendangnhap";
+            this.lblTendangnhap.Size = new System.Drawing.Size(124, 20);
+            this.lblTendangnhap.TabIndex = 2;
+            this.lblTendangnhap.Text = "Tên đăng nhập :";
+            // 
+            // lblMatkhau
+            // 
+            this.lblMatkhau.AutoSize = true;
+            this.lblMatkhau.Location = new System.Drawing.Point(244, 162);
+            this.lblMatkhau.Name = "lblMatkhau";
+            this.lblMatkhau.Size = new System.Drawing.Size(83, 20);
+            this.lblMatkhau.TabIndex = 3;
+            this.lblMatkhau.Text = "Mật khẩu :";
+            // 
+            // btnDangnhap
+            // 
+            this.btnDangnhap.Location = new System.Drawing.Point(332, 252);
+            this.btnDangnhap.Name = "btnDangnhap";
+            this.btnDangnhap.Size = new System.Drawing.Size(102, 43);
+            this.btnDangnhap.TabIndex = 4;
+            this.btnDangnhap.Text = "Đăng nhập";
+            this.btnDangnhap.UseVisualStyleBackColor = true;
+            this.btnDangnhap.Click += new System.EventHandler(this.btnDangnhap_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(464, 252);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(98, 43);
+            this.btnThoat.TabIndex = 5;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangnhap);
+            this.Controls.Add(this.lblMatkhau);
+            this.Controls.Add(this.lblTendangnhap);
+            this.Controls.Add(this.txtMatkhau);
+            this.Controls.Add(this.txtTendangnhap);
+            this.Name = "Form1";
+            this.Text = "Form Đăng Nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTendangnhap;
+        private System.Windows.Forms.TextBox txtMatkhau;
+        private System.Windows.Forms.Label lblTendangnhap;
+        private System.Windows.Forms.Label lblMatkhau;
+        private System.Windows.Forms.Button btnDangnhap;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormDangNhap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangnhap_Click(object sender, EventArgs e)
+        {
+            string tendangnhap = txtTendangnhap.Text;
+            string matkhau = txtMatkhau.Text;
+            if(tendangnhap == "admin"  && matkhau == "1234")
+            {
+                MessageBox.Show("Đăng nhập thành công");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại ");
+            }
+            txtTendangnhap.Text = "";
+            txtMatkhau.Text = "";
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
